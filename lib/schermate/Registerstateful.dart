0571d5276fe932e1455e0fcd89f19e1434d77fb1
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-03 19:18:36 +0200

    modificato stile pagina registrazione. !PROBLEMA OVERFLOW BOTTONE

--- lib/schermate/Registerstateful.dart
+++ lib/schermate/Registerstateful.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:testlogin/schermate/Loginstateful.dart';
+import 'package:testlogin/reusablewidgets/Textfieldregister.dart';
 
 class Registerstatteful extends StatefulWidget {
   const Registerstatteful({Key? key}) : super(key: key);
@@ -10,7 +12,7 @@ class Registerstatteful extends StatefulWidget {
 class _RegisterstattefulState extends State<Registerstatteful> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
+    /*return CustomScrollView(
       slivers: [
         SliverAppBar(
           floating: false,
@@ -29,10 +31,99 @@ class _RegisterstattefulState extends State<Registerstatteful> {
         ),
         SliverList(
           //da capire perchè dà problemi
-          delegate: SliverChildListDelegate([]),
+          delegate: SliverChildListDelegate([
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [TextField()],
+            )
+          ]),
         ),
         //),
       ],
+    );*/
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Registrati qui',
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
+        backgroundColor: Colors.pink[400],
+        leading: IconButton(
+            onPressed: () => {Navigator.pop(context, '/')},
+            icon: Icon(Icons.arrow_back)),
+      ),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SingleChildScrollView(
+            //scrollDirection: Axis.vertical,
+            padding: EdgeInsets.all(40),
+            child: Column(
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                /*Textfieldregister(   CAPIRE COME USARE MIO WIDGET CUSTOM
+                    labeltext: 'pippo',
+                  )*/
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.person)),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    labelText: 'Email',
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.android),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    labelText: 'Confirm password',
+                    prefixIcon: Icon(Icons.android),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                ElevatedButton(
+                  onPressed: () => {},
+                  child: Text('Registrati'),
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(140, 70),
+                      primary: Colors.pink[400],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)))),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
