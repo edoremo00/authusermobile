b990bd3b29e70f813aa2a8038377b57095fc8365
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-06 03:55:02 +0200

    rimosso codice non necessario e testata Bottomnavbar

--- lib/schermate/Registerstateful.dart
+++ lib/schermate/Registerstateful.dart
@@ -10,6 +10,7 @@ class Registerstatteful extends StatefulWidget {
 }
 
 class _RegisterstattefulState extends State<Registerstatteful> {
+  int selectedindex = 0;
   final usernamecontroller = TextEditingController();
   final emailcontroller = TextEditingController();
   final passwordcontroller = TextEditingController();
@@ -22,36 +23,21 @@ class _RegisterstattefulState extends State<Registerstatteful> {
         email: emailcontroller.text,
         password: passwordcontroller.text,
         confirmpassword: confirmpasswordcontroller.text);
-    /*return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          floating: false,
-          expandedHeight: 100,
-          pinned: true,
-          title: Text('Registrati'),
-          centerTitle: true,
-          backgroundColor: Colors.pink[400],
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () => {Navigator.pop(context, '/')}),
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text("Benvenuto"),
-            centerTitle: true,
-          ),
-        ),
-        SliverList(
-          //da capire perchè dà problemi
-          delegate: SliverChildListDelegate([
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [TextField()],
-            )
-          ]),
-        ),
-        //),
-      ],
-    );*/
     return Scaffold(
+      //# region Bottomnavbar
+      /*bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+
+              ),
+              label: 'Impostazioni')
+        ]
+      ),*/
+      //# end region
       appBar: AppBar(
         title: Text(
           'Registrati qui',
@@ -63,21 +49,20 @@ class _RegisterstattefulState extends State<Registerstatteful> {
             icon: Icon(Icons.arrow_back)),
       ),
       body: Column(
-        //mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Expanded(
             // wrappare expanded è fondamentale, senza non ho lo scrolling della pagina
             child: SingleChildScrollView(
-              //scrollDirection: Axis.vertical,
               padding:
                   EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
               child: Column(
                 children: [
                   Textfieldregister(
-                      labeltext: 'Username',
-                      prefixicon: Icon(Icons.person),
-                      textEditingController: usernamecontroller,
-                      textInputAction: TextInputAction.next),
+                    labeltext: 'Username',
+                    prefixicon: Icon(Icons.person),
+                    textEditingController: usernamecontroller,
+                    textInputAction: TextInputAction.next,
+                  ),
                   SizedBox(
                     height: 60,
                   ),
@@ -115,11 +100,14 @@ class _RegisterstattefulState extends State<Registerstatteful> {
                     onPressed: () => {reg.registeruser(reg)},
                     child: Text('Registrati'),
                     style: ElevatedButton.styleFrom(
-                        fixedSize: Size(140, 70),
-                        primary: Colors.pink[400],
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15)))),
+                      fixedSize: Size(140, 70),
+                      primary: Colors.pink[400],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                    ),
                   )
                 ],
               ),
