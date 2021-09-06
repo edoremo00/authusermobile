92c2f5d9cd41c0b9e3ce569d3454512bd730e287
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-06 19:40:37 +0200

    modificato stile appbar pagina register

--- lib/schermate/Registerstateful.dart
+++ lib/schermate/Registerstateful.dart
@@ -39,6 +39,12 @@ class _RegisterstattefulState extends State<Registerstatteful> {
       ),*/
       //# end region
       appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(48),
+          ),
+        ),
+        toolbarHeight: 70,
         title: Text(
           'Registrati qui',
           style: TextStyle(fontWeight: FontWeight.w500),
@@ -53,9 +59,11 @@ class _RegisterstattefulState extends State<Registerstatteful> {
           Expanded(
             // wrappare expanded è fondamentale, senza non ho lo scrolling della pagina
             child: SingleChildScrollView(
+              //primary: true,
               padding:
                   EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Textfieldregister(
                     labeltext: 'Username',
@@ -100,7 +108,7 @@ class _RegisterstattefulState extends State<Registerstatteful> {
                     onPressed: () => {reg.registeruser(reg)},
                     child: Text('Registrati'),
                     style: ElevatedButton.styleFrom(
-                      fixedSize: Size(140, 70),
+                      fixedSize: Size(160, 60),
                       primary: Colors.pink[400],
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.all(
