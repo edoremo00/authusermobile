892acd82622bc6cb8a56c3ffb914c73ccee1a8b6
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-07 19:56:59 +0200

    aggiunta appbar e menu a pagina gestione profilo

--- lib/schermate/gestioneprofilo.dart
+++ lib/schermate/gestioneprofilo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:testlogin/reusablewidgets/menu.dart';
+import 'package:testlogin/reusablewidgets/menureusable.dart';
 
 class Gestionprofilo extends StatefulWidget {
   Gestionprofilo({Key? key}) : super(key: key);
@@ -12,7 +13,48 @@ class _GestionprofiloState extends State<Gestionprofilo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Menu(),
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(48),
+          ),
+        ),
+        backgroundColor: Colors.pink[400],
+        toolbarHeight: 70,
+      ),
+      drawer: Drawer(
+        child: Material(
+          color: Colors.pink[200],
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 48,
+              ),
+              Reusablemenu(
+                titolo: 'Home',
+                coloreicona: Colors.black87,
+                coloretesto: Colors.black87,
+                funzioneontap: () => {},
+                grandezzatesto: 14,
+                iconaleading: Icons.home,
+                spessoretesto: FontWeight.bold,
+              ),
+              SizedBox(
+                height: 48,
+              ),
+              Reusablemenu(
+                titolo: 'Esci',
+                coloreicona: Colors.black87,
+                coloretesto: Colors.black87,
+                funzioneontap: () => {},
+                grandezzatesto: 14,
+                iconaleading: Icons.logout_rounded,
+                spessoretesto: FontWeight.bold,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
