334a5e6ba6d21c09748a639e96b2e58dcd410b0f
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-08 19:03:29 +0200

    istanza classe utente contenente info da mostrare

---
+++ lib/model/userdata.dart
@@ -0,0 +1,11 @@
+import 'package:testlogin/model/user.dart';
+
+class Userdata {
+  static const utente = User(
+      imagePath: 'https://images6.alphacoders.com/108/thumb-350-1081394.png',
+      email: 'creeper@creeper.com',
+      name: 'Edoardo',
+      surname: 'Remondini',
+      about: 'Just messing around with Flutter',
+      isDarkmode: false);
+}
