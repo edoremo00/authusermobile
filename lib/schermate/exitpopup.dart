4a8044e78fb6e1fefbb0413ab67a1ebc52577dc3
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-09 20:40:51 +0200

    aggiunto metodo per visualizzare alert ad uscita app

---
+++ lib/schermate/exitpopup.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+Future<bool> showExitPopup(context) async {
+  return await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Container(
+            height: 90,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Icon(Icons.exit_to_app_rounded),
+                Text("Uscire dall'APP?"),
+                //SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          print('yes selected');
+                          //exit(0);
+                          Navigator.of(context).pop();
+                          Navigator.of(context).pop(true);
+                        },
+                        style: ElevatedButton.styleFrom(primary: Colors.green),
+                        icon: Icon(Icons.check),
+                        label: Text('Conferma'),
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('no selected');
+                          Navigator.of(context).pop(false);
+                        },
+                        child: Text("Annulla",
+                            style: TextStyle(color: Colors.white)),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      });
+}
