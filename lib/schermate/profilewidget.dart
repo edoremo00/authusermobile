657160ac20bb000e18d059a0032e12c8676559af
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-12 23:05:57 +0200

    aggiunte property height e width a widget profilo

--- lib/schermate/profilewidget.dart
+++ lib/schermate/profilewidget.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class ProfileWidget extends StatelessWidget {
   final String imagePath;
   final VoidCallback onclick;
+  final double imagewidth;
+  final double imageheight;
 
-  const ProfileWidget({required this.imagePath, required this.onclick});
+  const ProfileWidget(
+      {required this.imagePath,
+      required this.onclick,
+      required this.imageheight,
+      required this.imagewidth});
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +41,8 @@ class ProfileWidget extends StatelessWidget {
         child: Ink.image(
           image: image,
           fit: BoxFit.cover,
-          width: 128,
-          height: 128,
+          width: imagewidth,
+          height: imageheight,
           child: InkWell(
             //grazie ad esso ho un effetto se clicco sopra l'immagine
             onTap: onclick,
