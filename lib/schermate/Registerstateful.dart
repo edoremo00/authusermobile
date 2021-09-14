22be253dcdcd0c8e911d8b0202dd7d259a80f3a1
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-14 23:33:44 +0200

    aggiunta validazione pagina register

--- lib/schermate/Registerstateful.dart
+++ lib/schermate/Registerstateful.dart
@@ -1,6 +1,8 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:testlogin/apiclasses/loginusermodel.dart';
 import 'package:testlogin/reusablewidgets/Textfieldregister.dart';
+import 'package:testlogin/schermate/LoginPage.dart';
 
 class Registerstatteful extends StatefulWidget {
   const Registerstatteful({Key? key}) : super(key: key);
@@ -10,14 +12,23 @@ class Registerstatteful extends StatefulWidget {
 }
 
 class _RegisterstattefulState extends State<Registerstatteful> {
-  int selectedindex = 0;
   final usernamecontroller = TextEditingController();
   final emailcontroller = TextEditingController();
   final passwordcontroller = TextEditingController();
   final confirmpasswordcontroller = TextEditingController();
 
   @override
+  void initState() {
+    // TODO: implement initState
+
+    //usernamecontroller.addListener(() => setState(() {}));
+    //emailcontroller.addListener(() => setState(() {}));
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final _formkey = GlobalKey<FormState>();
     Register reg = Register(
         username: usernamecontroller.text,
         email: emailcontroller.text,
@@ -56,75 +67,206 @@ class _RegisterstattefulState extends State<Registerstatteful> {
             onPressed: () => {Navigator.pop(context, '/')},
             icon: Icon(Icons.arrow_back)),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            // wrappare expanded è fondamentale, senza non ho lo scrolling della pagina
-            child: SingleChildScrollView(
-              //primary: true,
-              padding:
-                  EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Textfieldregister(
-                    labeltext: 'Username',
-                    prefixicon: Icon(Icons.person),
-                    textEditingController: usernamecontroller,
-                    textInputAction: TextInputAction.next,
-                  ),
-                  SizedBox(
-                    height: 60,
-                  ),
-                  Textfieldregister(
-                    labeltext: 'E-mail',
-                    prefixicon: Icon(Icons.email),
-                    textInputType: TextInputType.emailAddress,
-                    textEditingController: emailcontroller,
-                    textInputAction: TextInputAction.next,
-                  ),
-                  SizedBox(
-                    height: 60,
-                  ),
-                  Textfieldregister(
-                    labeltext: 'Password',
-                    prefixicon: Icon(Icons.lock),
-                    obscuretext: true,
-                    textInputAction: TextInputAction.next,
-                    textEditingController: passwordcontroller,
-                  ),
-                  SizedBox(
-                    height: 60,
-                  ),
-                  Textfieldregister(
-                    labeltext: 'Confirm password',
-                    prefixicon: Icon(Icons.lock),
-                    obscuretext: true,
-                    textInputAction: TextInputAction.done,
-                    textEditingController: confirmpasswordcontroller,
-                  ),
-                  SizedBox(
-                    height: 80,
-                  ),
-                  ElevatedButton(
-                    onPressed: () => {reg.registeruser(reg)},
-                    child: Text('Registrati'),
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: Size(160, 60),
-                      primary: Color.fromARGB(255, 52, 156, 225),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
+      body: Form(
+        key: _formkey,
+        child: Column(
+          children: [
+            Expanded(
+              // wrappare expanded è fondamentale, senza non ho lo scrolling della pagina
+              child: SingleChildScrollView(
+                //primary: true,
+                padding:
+                    EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Textfieldregister(
+                      labeltext: 'Username',
+                      prefixicon: Icon(Icons.person),
+                      textEditingController: usernamecontroller,
+                      textInputAction: TextInputAction.next,
+                      suffixicon: //usernamecontroller.text.isEmpty
+                          //? Container(
+                          // width: 0,
+                          //)
+                          //:
+                          IconButton(
+                        onPressed: () => (usernamecontroller.clear()),
+                        icon: Icon(Icons.close),
+                      ),
+                      validator: (username) {
+                        if (username!.isEmpty) {
+                          return 'inserisci un username';
+                        } else if (username.startsWith(RegExp(r'[0-9]'))) {
+                          return 'username non può iniziare con dei numeri';
+                        } else if (username.contains(' ')) {
+                          return 'username non può contenere spazi';
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Textfieldregister(
+                      labeltext: 'E-mail',
+                      prefixicon: Icon(Icons.email),
+                      textInputType: TextInputType.emailAddress,
+                      textEditingController: emailcontroller,
+                      textInputAction: TextInputAction.next,
+                      validator: (email) {
+                        if (email!.isEmpty) {
+                          return 'inserisci una mail';
+                        } else if (validateemail(email) == false) {
+                          return 'inserisci una mail valida';
+                        } else if (email.isEmpty) {
+                          return 'il campo mail non può essere vuoto';
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Textfieldregister(
+                      labeltext: 'Password',
+                      prefixicon: Icon(Icons.lock),
+                      obscuretext: true,
+                      textInputAction: TextInputAction.next,
+                      textEditingController: passwordcontroller,
+                      validator: (password) {
+                        if (password!.isEmpty) {
+                          return 'inserisci una password';
+                        } else if (password.contains(' ')) {
+                          return 'la password non può contenere spazi';
+                        } else if (validateStructure(password) == false) {
+                          Flushbar(
+                            icon: Icon(
+                              Icons.dangerous,
+                              color: Colors.black,
+                            ),
+                            backgroundColor: Colors.redAccent,
+                            message:
+                                'la password deve avere una lettera maiuscola dei numeri un carattere speciale ed essere lunga almeno 8 caratteri',
+                            title:
+                                'la password non rispetta i seguenti requisiti',
+                            duration: Duration(seconds: 4),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                            margin: EdgeInsets.all(10),
+                            //flushbarPosition: FlushbarPosition.TOP,
+                          ).show(context);
+                          return '';
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Textfieldregister(
+                      labeltext: 'Confirm password',
+                      prefixicon: Icon(Icons.lock),
+                      obscuretext: true,
+                      textInputAction: TextInputAction.done,
+                      textEditingController: confirmpasswordcontroller,
+                      validator: (confirmpassword) {
+                        if (confirmpassword!.isEmpty) {
+                          return 'inserisci conferma password';
+                        } else if (confirmpassword.contains(' ')) {
+                          return 'la password non può contenere spazi';
+                        } else if (validateStructure(confirmpassword) ==
+                            false) {
+                          Flushbar(
+                            icon: Icon(
+                              Icons.dangerous,
+                              color: Colors.black,
+                            ),
+                            backgroundColor: Colors.redAccent,
+                            message:
+                                'la password deve avere una lettera maiuscola dei numeri un carattere speciale ed essere lunga almeno 8 caratteri',
+                            title:
+                                'la password non rispetta i seguenti requisiti',
+                            duration: Duration(seconds: 4),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                            margin: EdgeInsets.all(10),
+                            //flushbarPosition: FlushbarPosition.TOP,
+                          ).show(context);
+                          return '';
+                        } else if (passwordmatch(
+                                passwordcontroller.text, confirmpassword) ==
+                            false) {
+                          return 'le due password non corrispondono';
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 80,
+                    ),
+                    ElevatedButton(
+                      onPressed: () async => {
+                        if (_formkey.currentState!.validate() == false)
+                          {}
+                        else
+                          {
+                            await Flushbar(
+                              title: 'Thank you',
+                              message: 'Processing data...',
+                              icon: Icon(
+                                Icons.info,
+                                color: Colors.black,
+                              ),
+                              duration: Duration(seconds: 3),
+                              backgroundColor: Colors.blueAccent,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                              margin: EdgeInsets.all(10),
+                            ).show(context).whenComplete(
+                                  () => reg.registeruser(reg),
+                                ),
+                          },
+                      },
+                      child: Text('Registrati'),
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(160, 60),
+                        primary: Color.fromARGB(255, 52, 156, 225),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+bool validateemail(String email) {
+  String pattern =
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{;|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+  RegExp regExp = RegExp(pattern);
+  return regExp.hasMatch(email);
+}
+
+bool passwordmatch(String password, String confirmpassword) {
+  if (password.toLowerCase() == confirmpassword.toLowerCase()) {
+    return true;
+  }
+  return false;
 }
