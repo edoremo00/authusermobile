c67f7963467183e934c2be26d609116b43ed496b
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-15 20:39:44 +0200

    aggiunti metodi per salvare, convertire oggetto user in json e salvarlo in shared preference e convertirlo da json a oggetto

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -3,14 +3,45 @@ class User {
   final String email;
   final String surname;
   final String about;
-  final bool isDarkmode;
-  final String name;
 
-  const User(
-      {required this.imagePath,
-      required this.email,
-      required this.name,
-      required this.surname,
-      required this.about,
-      required this.isDarkmode});
+  String name;
+
+  User({
+    required this.imagePath,
+    required this.email,
+    required this.name,
+    required this.surname,
+    required this.about,
+  });
+
+  Map<String, dynamic> toJson() => {
+        //per convertire oggetto utente a json per salvarlo in sharedpreferences
+        'imagePath': imagePath,
+        'email': email,
+        'surname': surname,
+        'about': about,
+        'name': name
+      };
+
+  User copy({
+    String? imagePath,
+    String? email,
+    String? surname,
+    String? about,
+    String? name,
+  }) =>
+      User(
+          imagePath: imagePath ??
+              this.imagePath, //?? significa che se è null uso valore esistente
+          email: email ?? this.email,
+          name: name ?? this.name,
+          surname: surname ?? this.surname,
+          about: about ?? this.about);
+
+  static User fromJson(Map<String, dynamic> user) => User(
+      about: user['about'],
+      email: user['email'],
+      imagePath: user['imagePath'],
+      name: user['name'],
+      surname: user['surname']);
 }
--- lib/model/userdata.dart
+++ lib/model/userdata.dart
@@ -1,11 +1,40 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:testlogin/model/user.dart';
 
 class Userdata {
-  static const utente = User(
-      imagePath: 'https://images6.alphacoders.com/108/thumb-350-1081394.png',
-      email: 'creeper@creeper.com',
-      name: 'Edoardo',
-      surname: 'Remondini',
-      about: 'Just messing around with Flutter',
-      isDarkmode: false);
+  static late SharedPreferences _preferences;
+  static const _keyuser = 'user'; //avrà info utente
+  static var utente = User(
+    imagePath: 'https://images6.alphacoders.com/108/thumb-350-1081394.png',
+    email: 'creeper@creeper.com',
+    name: 'Edoardo',
+    surname: 'Remondini',
+    about: 'Just messing around with Flutter',
+  );
+
+  static Future init() async =>
+      _preferences = await SharedPreferences.getInstance();
+
+  static Future setUser(User user) async {
+    //questo metodo serve per salvare i dati nello shared preferences
+    final userjson = jsonEncode(user
+        .toJson()); //devo fare encode del json per ricevere stringa però invece di un map
+    //salverò questa stringa nello shared preferences
+    await _preferences.setString(_keyuser,
+        userjson); //valore chiave è dato da json encodato(stringa), la chiave è usata per salvare valori su disco
+  }
+
+  static User getUser() {
+    final user = _preferences.getString(_keyuser);
+    //uso la chiave per recuperare dati utente
+
+    return user == null
+        ? utente
+        : User.fromJson(jsonDecode(
+            //ho oggetto in stringa dato da jsonencode.con jsondecode lo riporto a JSON.poi con from json leggi json e istanzi oggetto user prendendo valori da Map
+            //prima devo decodificare json prima di trasformarlo in ogetto User
+            user)); //questo metodo serve per convertire stringa ricevuta da sharedpreference a oggetto User
+  }
 }
