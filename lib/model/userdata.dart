df06c04248bd5f5c52f0df1e6e112e5bc7d976dc
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-16 14:38:02 +0200

    refactoring

--- lib/model/userdata.dart
+++ lib/model/userdata.dart
@@ -6,7 +6,7 @@ import 'package:testlogin/model/user.dart';
 class Userdata {
   static late SharedPreferences _preferences;
   static const _keyuser = 'user'; //avrà info utente
-  static var utente = User(
+  static const utente = User(
     imagePath: 'https://images6.alphacoders.com/108/thumb-350-1081394.png',
     email: 'creeper@creeper.com',
     name: 'Edoardo',
@@ -30,11 +30,9 @@ class Userdata {
     final user = _preferences.getString(_keyuser);
     //uso la chiave per recuperare dati utente
 
-    return user == null
-        ? utente
-        : User.fromJson(jsonDecode(
-            //ho oggetto in stringa dato da jsonencode.con jsondecode lo riporto a JSON.poi con from json leggi json e istanzi oggetto user prendendo valori da Map
-            //prima devo decodificare json prima di trasformarlo in ogetto User
-            user)); //questo metodo serve per convertire stringa ricevuta da sharedpreference a oggetto User
+    return user == null ? utente : User.fromJson(jsonDecode(
+        //ho oggetto in stringa dato da jsonencode.con jsondecode lo riporto a JSON.poi con from json leggi json e istanzi oggetto user prendendo valori da Map
+        //prima devo decodificare json prima di trasformarlo in ogetto User
+        user)); //questo metodo serve per convertire stringa ricevuta da sharedpreference a oggetto User
   }
 }
