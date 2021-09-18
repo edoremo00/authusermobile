95c3fd3d12e7c1ae75b64e2fc9697da43af69d4b
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-18 02:46:38 +0200

    creato dialog conferma se si esce mentre si modifcano infoutente

---
+++ lib/schermate/edituserdiscardpopup.dart
@@ -0,0 +1,25 @@
+/*import 'package:flutter/material.dart';
+
+Future<bool> editUserdiscardpopup(context) async {
+  return await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Annullare le modifiche? ogni cambiamento verrà perso'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text('Conferma'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text('Annulla'),
+            ),
+          ],
+        );
+      });
+}*/
