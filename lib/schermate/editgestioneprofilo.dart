e1e29a97b920f006c7725c9f5fd10ab12747beea
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-10-24 00:04:38 +0200

    aggiunto dialog per scegliere foto da galleria o scattarne una nuova+ FIX SHPREFERENCES+animazione loading bottone

--- lib/schermate/editgestioneprofilo.dart
+++ lib/schermate/editgestioneprofilo.dart
@@ -33,25 +33,70 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
   final _snackBar = SnackBar(content: Text('Profilo aggiornato con successo'));
   late bool isloading;
   late bool cameraselected;
+  late bool didntchoseoption;
+  late bool savebuttonenabled;
 
   @override
   void initState() {
     super.initState();
+    Userdata.getUser();
     usernamecontroller = TextEditingController(text: u.name);
     surnamecontroller = TextEditingController(text: u.surname);
     emailcontroller = TextEditingController(text: u.email);
     aboutcontroller = TextEditingController(text: u.about);
-    //isbuttondisabled = false;
 
     _formkey = GlobalKey<FormState>();
     isloading = false;
     cameraselected = false;
+    didntchoseoption = true;
+    savebuttonenabled = false;
+    //per evitare problema in cui utente chiude dialog senza fare nulla
+
+    /*usernamecontroller.addListener(() {
+      setState(() {
+        if (usernamecontroller.value.text.toLowerCase().trim() !=
+            u.name.toLowerCase().trim())
+          savebuttonenabled = true;
+        else
+          savebuttonenabled = false;
+      });
+    });*/
+    /*surnamecontroller.addListener(() {
+      setState(() {
+        if (surnamecontroller.value.text.toLowerCase().trim() !=
+            u.surname.toLowerCase().trim())
+          savebuttonenabled = true;
+        else
+          savebuttonenabled = false;
+      });
+    });
+    emailcontroller.addListener(() {
+      setState(() {
+        if (emailcontroller.value.text.toLowerCase().trim() !=
+            u.email.toLowerCase().trim())
+          savebuttonenabled = true;
+        else
+          savebuttonenabled = false;
+      });
+    });
+    aboutcontroller.addListener(() {
+      setState(() {
+        if (aboutcontroller.value.text.toLowerCase().trim() !=
+            u.about.toLowerCase().trim()) {
+          savebuttonenabled = true;
+        } else {
+          savebuttonenabled = false;
+        }
+      });
+    });*/
   }
 
   @override
   void dispose() {
     usernamecontroller.dispose();
     surnamecontroller.dispose();
+    emailcontroller.dispose();
+    aboutcontroller.dispose();
     super.dispose();
   }
 
@@ -73,8 +118,10 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
             ProfileWidget(
               imagePath: u.imagePath,
               onclick: () async {
-                await selectpicturemessage(
-                    context); //problema se utente chiude dialog ritorna null
+                didntchoseoption = true;
+                await selectpicturemessage(context);
+                if (didntchoseoption == true)
+                  return; //problema se utente chiude dialog ritorna null
                 final image = await ImagePicker().pickImage(
                     source: cameraselected
                         ? ImageSource.camera
@@ -87,15 +134,16 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
                     .path); // con basename ottengo solo nome file immagine scelta
                 final imagefile = // questo sarà il path completo della nuova immagine. dato da directory + basename
                     File('${directorysalvafoto.path}/$imagenamefile');
-                final nuovaimmagine = await File(image.path).copy(imagefile
-                    .path); //creo un file con path immagine e lo copio nella cartella
-                setState(() => u =
-                    //u.copy(imagePath: nuovaimmagine.path);
-
-                    u.copy(
-                        imagePath: nuovaimmagine
-                            .path)); //sovrascrivo nuove property a oggetto user e aggiorno UI con setstate
-                //u.imagePath = nuovaimmagine.path;
+                final nuovaimmagine =
+                    await File(image.path).copy(imagefile.path);
+                //creo un file con path immagine e lo copio nella cartella
+                //aggiorno UI PER MOSTRARE FOTO
+                setState(
+                  () => u = u.copy(imagePath: nuovaimmagine.path),
+                );
+                await Userdata.setUser(
+                  u.copy(imagePath: nuovaimmagine.path),
+                ); //SALVO NUOVA FOTO IN SHPREFERENCES
               },
               imageheight: 128,
               imagewidth: 128,
@@ -105,9 +153,16 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
             Editprofiletextwidget(
               label: 'Username',
               text: u.name,
-              onchanged: (name) => u = u.copy(
-                  name:
-                      name), //metodo copy sovrascrive oggetto user se ci sono cambiamenti. altrimenti usa valori oggetto esistente
+              //onchanged: (name) => u = u.copy(
+              onchanged: (name) => setState(() {
+                if (usernamecontroller.value.text.toLowerCase().trim() !=
+                    u.name.toLowerCase().trim())
+                  savebuttonenabled = true;
+                else
+                  savebuttonenabled = false;
+              }),
+              //name:
+              // name), //metodo copy sovrascrive oggetto user se ci sono cambiamenti. altrimenti usa valori oggetto esistente
               controller: usernamecontroller,
               validator: (username) {
                 if (username!.isEmpty) {
@@ -127,7 +182,13 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
             Editprofiletextwidget(
               label: 'Surname',
               text: u.surname,
-              onchanged: (surname) => u = u.copy(surname: surname),
+              onchanged: (surname) => setState(() {
+                if (surnamecontroller.value.text.toLowerCase().trim() !=
+                    u.surname.toLowerCase().trim())
+                  savebuttonenabled = true;
+                else
+                  savebuttonenabled = false;
+              }),
               controller: surnamecontroller,
               validator: (surname) {
                 if (surname!.isEmpty) {
@@ -147,7 +208,13 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
             Editprofiletextwidget(
               label: 'email',
               text: u.email,
-              onchanged: (email) => u = u.copy(email: email),
+              onchanged: (email) => setState(() {
+                if (emailcontroller.value.text.toLowerCase().trim() !=
+                    u.email.toLowerCase().trim())
+                  savebuttonenabled = true;
+                else
+                  savebuttonenabled = false;
+              }),
               controller: emailcontroller,
               validator: (email) {
                 if (email!.isEmpty) {
@@ -166,8 +233,14 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
             ),
             Editprofiletextwidget(
               label: 'About',
+              onchanged: (about) => setState(() {
+                if (aboutcontroller.value.text.toLowerCase().trim() !=
+                    u.about.toLowerCase().trim())
+                  savebuttonenabled = true;
+                else
+                  savebuttonenabled = false;
+              }),
               text: u.about,
-              onchanged: (about) => u = u.copy(about: about),
               controller: aboutcontroller,
               maxlines: 5,
               validator: (about) {
@@ -182,32 +255,33 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
               height: 24,
             ),
             ElevatedButton.icon(
-              onPressed: () async {
-//METODO PRECEDENTE
-                //AGGIUSTARE METODO useralredyinsharedpreferences()
-                if (_formkey.currentState!.validate() == false) {
-                  //await useralredyinsharedpreferences();
-                  //u == defaultuser ho implementato equatable per capire se oggetto salvato è uguale a quello predefinito. se lo è non salvo
-                  //se form non è valido non sovrascrivo oggetto
-
-                } else {
-                  setState(() {
-                    isloading = !isloading;
-                  });
-                  await Future.delayed(Duration(seconds: 3)).whenComplete(
-                    () => setState(
-                      () {
-                        isloading = false;
-                      },
-                    ),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(_snackBar);
-                  /*await Future.delayed(const Duration(seconds: 5), () {
-                    ScaffoldMessenger.of(context).clearSnackBars();
-                  });*/
-                  await Userdata.setUser(u);
-                }
-              }, //u.name = usernamecontroller.text => PER MODIFICARE CAMPI UTENTE, BISOGNA TOGLIERE TUTTI I CAMPI FINAL PER FARLO FUNZIONARE. INOLTRE DATI NON SI AGGIORNANO SUBITO
+              onPressed: savebuttonenabled && _formkey.currentState!.validate()
+                  ? () async {
+                      //AGGIUSTARE METODO useralredyinsharedpreferences()
+                      if (_formkey.currentState!.validate() == false) {
+                        //se form non è valido non sovrascrivo oggetto
+                      } else {
+                        setState(() {
+                          isloading = !isloading;
+                        });
+                        await Future.delayed(Duration(seconds: 3)).whenComplete(
+                          () => setState(
+                            () {
+                              isloading = false;
+                            },
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+                        await Userdata.setUser(
+                          u.copy(
+                              about: aboutcontroller.value.text,
+                              email: emailcontroller.value.text,
+                              name: usernamecontroller.value.text,
+                              surname: surnamecontroller.value.text),
+                        );
+                      }
+                    }
+                  : null, //u.name = usernamecontroller.text => PER MODIFICARE CAMPI UTENTE, BISOGNA TOGLIERE TUTTI I CAMPI FINAL PER FARLO FUNZIONARE. INOLTRE DATI NON SI AGGIORNANO SUBITO
               icon: isloading
                   ? Row(
                       mainAxisAlignment: MainAxisAlignment.start,
@@ -221,7 +295,10 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
                   : Icon(Icons.save), //Icon(Icons.save),
               label: isloading ? Text('Attendere prego') : Text('Salva'),
               style: ElevatedButton.styleFrom(
-                  primary: Color.fromARGB(255, 52, 156, 225),
+                  onSurface: Colors.blue,
+                  primary: isloading
+                      ? Color.fromARGB(255, 0, 187, 255)
+                      : Color.fromARGB(255, 52, 156, 225),
                   fixedSize: Size(200, 55),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(
@@ -264,7 +341,7 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
     return Future.value(false);
   }
 
-  Future<dynamic> selectpicturemessage(BuildContext context) async {
+  Future<void> selectpicturemessage(BuildContext context) async {
     return await showDialog(
         context: context,
         builder: (context) {
@@ -272,24 +349,54 @@ class _EditgestioneprofiloState extends State<Editgestioneprofilo> {
             content: Container(
               child: Row(
                 children: [
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      cameraselected = false;
-                      Navigator.pop(context, true); //per chiudere dialog
-                    },
-                    icon: Icon(Icons.photo_size_select_actual_outlined),
-                    label: Text('Gallery'),
+                  Expanded(
+                    child: OutlinedButton.icon(
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Colors.blue, width: 1),
+                      ),
+                      onPressed: () {
+                        cameraselected = false;
+                        didntchoseoption = false;
+                        Navigator.pop(context, true); //per chiudere dialog
+                      },
+                      icon: Icon(
+                        Icons.photo_size_select_actual_outlined,
+                        color: Colors.blue[400],
+                      ),
+                      label: Text(
+                        'Gallery',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
                   ),
                   SizedBox(
                     width: 10,
                   ),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      cameraselected = true;
-                      Navigator.pop(context, true);
-                    },
-                    icon: Icon(Icons.camera),
-                    label: Text("Create new"),
+                  Expanded(
+                    child: OutlinedButton.icon(
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Colors.blue, width: 1),
+                      ),
+                      onPressed: () {
+                        cameraselected = true;
+                        didntchoseoption = false;
+                        Navigator.pop(context, true);
+                      },
+                      icon: Icon(
+                        Icons.camera,
+                        color: Colors.blue[400],
+                      ),
+                      label: Text(
+                        "Camera",
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
                   )
                 ],
               ),
